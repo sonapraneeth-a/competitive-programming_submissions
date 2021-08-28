b89780f81415e61656446d21fe3f0fe4c7c954d0
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2021-08-28 18:49:15 +0530

    feat: added initial problem and 4 solutions for 1. Two Sum problem
    Signed-off-by: Sona Praneeth Akula <sonapraneeth.akula@gmail.com>

--- platforms/leetcode/practice/1__two-sum/description.md
+++ platforms/leetcode/practice/1__two-sum/description.md
@@ -2,83 +2,60 @@
 Problem Title: 1. Two Sum
 Problem Difficulty: Easy
 Problem Url: https://leetcode.com/problems/two-sum/
-Problem Tags: Array, Hash Table
+Problem Tags: Array, Two pointers
 Solution Url: https://leetcode.com/problems/two-sum/solution/
 IsPremium: No
-IsTemplate: No
+IsTemplate: Yes
 ---
 
 <span style="color: rgb(67, 160, 71);">Easy</span>
 
 # Description
 
-Given an array of integers `nums` and an integer `target`, return *indices of the two numbers such that they add up to `target`*.
+Given an array of integers `nums` and an integer `target`, return _indices of
+the two numbers such that they add up to `target`_.
 
-
-You may assume that each input would have ***exactly* one solution**, and you may not use the *same* element twice.
-
+You may assume that each input would have **_exactly_ one solution**, and you
+may not use the _same_ element twice.
 
 You can return the answer in any order.
 
-
- 
-
-
 **Example 1:**
 
-
-
-```
-
+```markdown
 **Input:** nums = [2,7,11,15], target = 9
 **Output:** [0,1]
-**Output:** Because nums[0] + nums[1] == 9, we return [0, 1].
-
+**Explanation:** Because nums[0] + nums[1] == 9, we return [0, 1].
 ```
 
 **Example 2:**
 
-
-
-```
-
+```markdown
 **Input:** nums = [3,2,4], target = 6
 **Output:** [1,2]
-
 ```
 
 **Example 3:**
 
-
-
-```
-
+```markdown
 **Input:** nums = [3,3], target = 6
 **Output:** [0,1]
-
 ```
-
- 
-
 
 **Constraints:**
 
+-   `2 <= nums.length <= 10^4`
+-   `-10^9 <= nums[i] <= 10^9`
+-   `-10^9 <= target <= 10^9`
+-   **Only one valid answer exists.**
 
-* `2 <= nums.length <= 104`
-* `-109 <= nums[i] <= 109`
-* `-109 <= target <= 109`
-* **Only one valid answer exists.**
-
-
- 
-
-
-**Follow-up:**Can you come up with an algorithm that is less than `O(n2)`time complexity?
+**Follow-up:** Can you come up with an algorithm that is less than `O(n^2)`
+time complexity?
 
 # LeetCode Similar Problems
 
-- []()
+-   []()
 
 # Useful Discussions
 
-- []()
+-   []()
--- platforms/leetcode/practice/1__two-sum/solutions.cpp
+++ platforms/leetcode/practice/1__two-sum/solutions.cpp
@@ -26,8 +26,19 @@
  *
  * Date (DD-MM-YYYY)            Author               Update
  * 27-08-2021               sonapraneeth_a      - Creation of file
+ *                                              - Adding 4 solutions
  */
 // clang-format on
+
+#include <algorithm>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+using std::pair;
+using std::sort;
+using std::unordered_map;
+using std::vector;
 
 namespace solution_01 {
 // clang-format off
@@ -38,28 +49,242 @@ namespace solution_01 {
  * Status: .  /  test cases passed.
  * Runtime:  ms, faster than % of C++ online submissions
  * Memory usage:  MB, less than % of C++ online submissions
- * Time complexity:
- * Space complexity:
- * Tags:
- * Categories:
- * Additional notes
+ * Time complexity: O(n^2), where n is size of nums
+ * Space complexity: O(1)
+ * Tags: Brute force
+ * Categories: Array
+ * Additional notes:
  *  -
  */
 // clang-format on
+class Solution {
+ public:
+    // clang-format off
+    /**
+     * FUNCTION DESCRIPTION
+     *
+     * Time complexity: O(n^2), where n is size of nums
+     * Space complexity: O(1)
+     * Additional notes
+     *  -
+     */
+    // clang-format on
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int nums_sz = static_cast<int>(nums.size());
+        // 0. Base case
+        if (nums_sz < 2) {
+            return {};
+        }
+        // 1. For all pairs of numbers, find that pair which sums to `target`
+        for (int nums_i = 0; nums_i < nums_sz; ++nums_i) {
+            for (int nums_j = nums_i + 1; nums_j < nums_sz; ++nums_j) {
+                if (nums[nums_i] + nums[nums_j] == target) {
+                    return {nums_i, nums_j};
+                }
+            }
+        }
+        return {};
+    }
+};
+}  // namespace solution_01
+
+namespace solution_02 {
 // clang-format off
 /**
- * FUNCTION DESCRIPTION
+ * CLASS DESCRIPTION
  *
- * Time complexity:
- * Space complexity:
+ * Submission link: https://leetcode.com/submissions/detail//
+ * Status: .  /  test cases passed.
+ * Runtime:  ms, faster than % of C++ online submissions
+ * Memory usage:  MB, less than % of C++ online submissions
+ * Time complexity: O(n*log(n) + n) ~ O(n*log(n)), where n is size of nums
+ * Space complexity: O(n), where n is size of nums
+ * Tags: Sort, Two pointers
+ * Categories: Array
  * Additional notes
  *  -
  */
 // clang-format on
 class Solution {
-public:
+ public:
+    // clang-format off
+    /**
+     * FUNCTION DESCRIPTION
+     *
+     * Time complexity: O(n*log(n) + n) ~ O(n*log(n)), where n is size of nums
+     * Space complexity: O(n), where n is size of nums
+     * Additional notes
+     *  -
+     */
+    // clang-format on
     vector<int> twoSum(vector<int>& nums, int target) {
-        
+        int nums_sz = static_cast<int>(nums.size());
+        // 0. Base case
+        if (nums_sz < 2) {
+            return {};
+        }
+        // 1. Create a map of number and its corresponding index in `nums`
+        vector<pair<int, int>> numbers(nums_sz, {-1, -1});
+        for (int nums_i = 0; nums_i < nums_sz; ++nums_i) {
+            numbers[nums_i] = {nums[nums_i], nums_i};
+        }
+        // 2. Sort the numbers in ascending order first based on value and then
+        //    based on index
+        sort(begin(numbers), end(numbers),
+             [](const pair<int, int>& lhs, const pair<int, int>& rhs) {
+                 if (lhs.first == rhs.first) {
+                     return lhs.second < rhs.second;
+                 }
+                 return lhs.first < rhs.first;
+             });
+        // 3. Use two pointers, `left_nums_i` (pointing towards the left end of
+        //    sorted array) and `right_nums_i` (pointing towards the right end
+        //    of sorted array)
+        int left_nums_i  = 0;
+        int right_nums_i = nums_sz - 1;
+        // 4. Calculate the sum of `nums[left_nums_i]` and `nums[right_nums_i]`
+        //    Let it be `sum`. Check if the `sum` equals `target`
+        //    If yes, we've got the required indices
+        //    If no,
+        //      (1) If the `sum` > `target`, in order to reached the target
+        //          we need to reduce `right_nums_i`, so that the new `sum` can
+        //          be reduced
+        //      (2) If the `sum` < `target`, in order to reached the target
+        //          we need to increase `left_nums_i`, so that the new `sum` can
+        //          be increased
+        //    Continue this until `left_nums_i` < `right_nums_i`
+        while (left_nums_i < right_nums_i) {
+            if (numbers[left_nums_i].first + numbers[right_nums_i].first ==
+                target) {
+                return {numbers[left_nums_i].second,
+                        numbers[right_nums_i].second};
+            }
+            if (numbers[left_nums_i].first + numbers[right_nums_i].first >
+                target) {
+                --right_nums_i;
+            } else {
+                ++left_nums_i;
+            }
+        }
+        return {};
     }
 };
-}  // namespace solution_01
+}  // namespace solution_02
+
+namespace solution_03 {
+// clang-format off
+/**
+ * CLASS DESCRIPTION
+ *
+ * Submission link: https://leetcode.com/submissions/detail//
+ * Status: .  /  test cases passed.
+ * Runtime:  ms, faster than % of C++ online submissions
+ * Memory usage:  MB, less than % of C++ online submissions
+ * Time complexity: O(n+n) ~ O(n), where n is size of nums
+ * Space complexity: O(n), where n is size of nums
+ * Tags: Two pass
+ * Categories: Array, Hashmap
+ * Additional notes
+ *  -
+ */
+// clang-format on
+class Solution {
+ public:
+    // clang-format off
+    /**
+     * FUNCTION DESCRIPTION
+     *
+     * Time complexity: O(n+n) ~ O(n), where n is size of nums
+     * Space complexity: O(n), where n is size of nums
+     * Additional notes
+     *  -
+     */
+    // clang-format on
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int nums_sz = static_cast<int>(nums.size());
+        // 0. Base case
+        if (nums_sz < 2) {
+            return {};
+        }
+        // 1. Create a mapping from number to its occurrence in `nums`. Store it
+        //    in `vector` as a single number could occur multiple times in
+        //    `nums`
+        unordered_map<int, vector<int>> num_to_nums_i_map {};
+        for (int nums_i = 0; nums_i < nums_sz; ++nums_i) {
+            num_to_nums_i_map[nums[nums_i]].emplace_back(nums_i + 1);
+        }
+        // 2. Loop through map
+        //  (1) If `target` == (2 * `key`) and we have atleast two instances of
+        //     `key` occurring in `nums`, we've got the required indices
+        //  (2) Check if `key` and `target - key` are present in `nums`. If yes,
+        //      we've got the required indices
+        for (const auto& [key, val] : num_to_nums_i_map) {
+            if (target == (key * 2)) {
+                if (val.size() >= 2) {
+                    return {val[0] - 1, val[1] - 1};
+                }
+            } else {
+                auto itr = num_to_nums_i_map.find(target - key);
+                if (itr != num_to_nums_i_map.end()) {
+                    return {val[0] - 1, itr->second[0] - 1};
+                }
+            }
+        }
+        return {};
+    }
+};
+}  // namespace solution_03
+
+namespace solution_04 {
+// clang-format off
+/**
+ * CLASS DESCRIPTION
+ *
+ * Submission link: https://leetcode.com/submissions/detail//
+ * Status: .  /  test cases passed.
+ * Runtime:  ms, faster than % of C++ online submissions
+ * Memory usage:  MB, less than % of C++ online submissions
+ * Time complexity: O(n), where n is size of nums
+ * Space complexity: O(n), where n is size of nums
+ * Tags: One pass
+ * Categories: Array, Hashmap
+ * Additional notes
+ *  -
+ */
+// clang-format on
+class Solution {
+ public:
+    // clang-format off
+    /**
+     * FUNCTION DESCRIPTION
+     *
+     * Time complexity: O(n), where n is size of nums
+     * Space complexity: O(n), where n is size of nums
+     * Additional notes
+     *  -
+     */
+    // clang-format on
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int nums_sz = static_cast<int>(nums.size());
+        // 0. Base case
+        if (nums_sz < 2) {
+            return {};
+        }
+        // 1. Create a mapping from number to its occurrence in `nums`.
+        // 2. For each `value` in `nums`
+        //    (1) Check if `target - value` exists in map. If yes, we've got
+        //        required indices
+        //    (2) Else, add `value` and its corresponding index to map
+        unordered_map<int, int> num_to_nums_i_map;
+        for (int nums_i = 0; nums_i < nums_sz; ++nums_i) {
+            auto itr = num_to_nums_i_map.find(target - nums[nums_i]);
+            if (itr == num_to_nums_i_map.end()) {
+                num_to_nums_i_map.insert({nums[nums_i], nums_i});
+            } else {
+                return {itr->second, nums_i};
+            }
+        }
+        return {};
+    }
+};
+}  // namespace solution_04
