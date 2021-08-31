6c92a40e640af0248290308fc8207d5edec07376
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2021-08-31 21:40:51 +0530

    chore: remove unecessary comments and add reverse parameter to script
    Signed-off-by: Sona Praneeth Akula <sonapraneeth.akula@gmail.com>

--- scripts/main__setup_default_code.py
+++ scripts/main__setup_default_code.py
@@ -19,13 +19,16 @@ if __name__ == "__main__":
                              "Default: LeetCode")
     parser.add_argument("-o", "--overwrite", action="store_true",
                         default=False,
-                        help="Overwrite files if the file is not updated or the"
+                        help="Overwrite files if the file is not updated or the "
                              "file does not exist. If True, overwites all files."
                              "Default: False")
     parser.add_argument("-c", "--count", type=int,
                         default=-1,
-                        help="Number of problems to retrieve. Default: -1."
+                        help="Number of problems to retrieve. Default: -1. "
                              "If -1, all problems are retrieved")
+    parser.add_argument("-r", "--reverse", action="store_true",
+                        default=True,
+                        help="Sort the retreived problems")
     parser.add_argument("-q", "--quiet", action="store_true",
                         default=False,
                         help="Suppress verbose messages. Default: False")
@@ -38,14 +41,14 @@ if __name__ == "__main__":
     if args.platform.lower() == "leetcode":
         leetcode_api = LeetCodeApi(quiet=args.quiet)
         algorithm_problems, _ = leetcode_api.get_algorithm_problems(force=False)
-        algorithm_problems.sort(key=lambda x: int(x.identifier), reverse=True)
+        algorithm_problems.sort(key=lambda x: int(x.identifier),
+                                reverse=args.reverse)
         number_of_problems = len(
             algorithm_problems) if args.count == -1 else args.count
         for problem in algorithm_problems[:number_of_problems]:
             print(problem)
         submit_directory = "..\\platforms\\leetcode\\practice"
         root = os.getcwd()
-        print("Setting up solutions.cpp")
         template_solutions_file_path = \
             "\\templates" + "\\leetcode\\" + "solutions.cpp"
         template_desc_file_path = \
@@ -53,13 +56,9 @@ if __name__ == "__main__":
         for problem in algorithm_problems[:number_of_problems]:
             if problem.is_premium:
                 continue
-            print("Setting up solutions.cpp for {0}. {1}"
-                  .format(problem.identifier, problem.title))
             create_file(
                 problem, submit_directory, "solutions.cpp",
                 root + template_solutions_file_path, overwrite=args.overwrite)
-            print("Setting up description.md for {0}. {1}"
-                  .format(problem.identifier, problem.title))
             create_file(
                 problem, submit_directory, "description.md",
                 root + template_desc_file_path, overwrite=args.overwrite)
