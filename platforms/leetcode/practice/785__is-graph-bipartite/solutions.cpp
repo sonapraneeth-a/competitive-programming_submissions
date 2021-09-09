40cda813dcb962a6b6ff04c12905bbd0f93a3e15
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2021-09-09 14:56:28 +0530

    feat: added solution to 785. Is Graph Bipartite? problem in leetcode
    Signed-off-by: Sona Praneeth Akula <sonapraneeth.akula@gmail.com>

--- platforms/leetcode/practice/785__is-graph-bipartite/solutions.cpp
+++ platforms/leetcode/practice/785__is-graph-bipartite/solutions.cpp
@@ -29,6 +29,18 @@
  */
 // clang-format on
 
+#include <memory>
+#include <queue>
+#include <unordered_set>
+#include <vector>
+
+using std::make_unique;
+using std::queue;
+using std::swap;
+using std::unique_ptr;
+using std::unordered_set;
+using std::vector;
+
 namespace solution_01 {
 // clang-format off
 /**
@@ -46,20 +58,242 @@ namespace solution_01 {
  *  -
  */
 // clang-format on
+class Solution {
+ public:
+    // clang-format off
+    /**
+     * FUNCTION DESCRIPTION
+     *
+     * Time complexity:
+     * Space complexity:
+     * Additional notes
+     *  -
+     */
+    // clang-format on
+    bool isBipartite(vector<vector<int>> &graph) {
+        int          number_of_nodes = static_cast<int>(graph.size());
+        vector<char> color(number_of_nodes, 'U');
+        for (int node_no = 0; node_no < number_of_nodes; ++node_no) {
+            if (color[node_no] != 'U') {
+                continue;
+            }
+            queue<int> bfs;
+            bfs.push(node_no);
+            color[node_no] = 'R';
+            while (!bfs.empty()) {
+                auto source = bfs.front();
+                bfs.pop();
+                char source_color = color[source];
+                for (const auto &neighbour : graph[source]) {
+                    if (color[neighbour] == source_color) {
+                        return false;
+                    }
+                    if (color[neighbour] == 'U') {
+                        color[neighbour] = source_color == 'R' ? 'B' : 'R';
+                        bfs.push(neighbour);
+                    }
+                }
+            }
+        }
+        return true;
+    }
+};
+}  // namespace solution_01
+
+namespace solution_02 {
 // clang-format off
 /**
- * FUNCTION DESCRIPTION
+ * CLASS DESCRIPTION
  *
+ * Submission link: https://leetcode.com/submissions/detail//
+ * Status: .  /  test cases passed.
+ * Runtime:  ms, faster than % of C++ online submissions
+ * Memory usage:  MB, less than % of C++ online submissions
  * Time complexity:
  * Space complexity:
+ * Tags:
+ * Categories:
  * Additional notes
  *  -
  */
 // clang-format on
 class Solution {
-public:
-    bool isBipartite(vector<vector<int>>& graph) {
-        
+ private:
+    bool dfs(vector<vector<int>> &graph, int source, unordered_set<int> &set_a,
+             unordered_set<int> &set_b) {
+        set_a.insert(source);
+        for (const auto &neighbour : graph[source]) {
+            // We cannot have source and neighbour in the same set
+            if (set_a.find(neighbour) != set_a.end()) {
+                return false;
+            }
+            if (set_b.find(neighbour) == set_b.end()) {
+                set_b.insert(neighbour);
+                if (!dfs(graph, neighbour, set_b, set_a)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+ public:
+    // clang-format off
+    /**
+     * FUNCTION DESCRIPTION
+     *
+     * Time complexity:
+     * Space complexity:
+     * Additional notes
+     *  -
+     */
+    // clang-format on
+    bool isBipartite(vector<vector<int>> &graph) {
+        unordered_set<int> set_a, set_b;
+        int                number_of_nodes = static_cast<int>(graph.size());
+        for (int node_no = 0; node_no < number_of_nodes; ++node_no) {
+            if (set_b.find(node_no) == set_b.end() &&
+                set_a.find(node_no) == set_a.end()) {
+                if (!dfs(graph, node_no, set_a, set_b)) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 };
-}  // namespace solution_01
+}  // namespace solution_02
+
+namespace solution_03 {
+// clang-format off
+/**
+ * CLASS DESCRIPTION
+ *
+ * Submission link: https://leetcode.com/submissions/detail//
+ * Status: .  /  test cases passed.
+ * Runtime:  ms, faster than % of C++ online submissions
+ * Memory usage:  MB, less than % of C++ online submissions
+ * Time complexity:
+ * Space complexity:
+ * Tags:
+ * Categories:
+ * Additional notes
+ *  -
+ */
+// clang-format on
+class Solution {
+ private:
+    bool dfs(vector<vector<int>> &graph, int source, vector<char> &color) {
+        for (const auto &neighbour : graph[source]) {
+            if (color[neighbour] == 'U') {
+                color[neighbour] = color[source] == 'R' ? 'B' : 'R';
+                if (!dfs(graph, neighbour, color)) {
+                    return false;
+                }
+            }
+            if (color[source] == color[neighbour]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+ public:
+    // clang-format off
+    /**
+     * FUNCTION DESCRIPTION
+     *
+     * Time complexity:
+     * Space complexity:
+     * Additional notes
+     *  -
+     */
+    // clang-format on
+    bool isBipartite(vector<vector<int>> &graph) {
+        int          number_of_nodes = static_cast<int>(graph.size());
+        vector<char> color(number_of_nodes, 'U');
+        for (int node_no = 0; node_no < number_of_nodes; ++node_no) {
+            if (color[node_no] == 'U') {
+                color[node_no] = 'R';
+                if (!dfs(graph, node_no, color)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
+}  // namespace solution_03
+
+namespace solution_04 {
+// clang-format off
+/**
+ * CLASS DESCRIPTION
+ *
+ * Submission link: https://leetcode.com/submissions/detail//
+ * Status: .  /  test cases passed.
+ * Runtime:  ms, faster than % of C++ online submissions
+ * Memory usage:  MB, less than % of C++ online submissions
+ * Time complexity:
+ * Space complexity:
+ * Tags:
+ * Categories: Disjoint set
+ * Additional notes
+ *  -
+ */
+// clang-format on
+class Solution {
+ private:
+    class DisjointSet {
+     private:
+        std::vector<int> _ds;
+
+     public:
+        explicit DisjointSet(int N) { _ds.resize(N, -1); }
+        // Path compression
+        int  find(int x) { return _ds[x] < 0 ? x : _ds[x] = find(_ds[x]); }
+        void union_parents(int parent_x, int parent_y) {
+            if (parent_x != parent_y) {
+                if (-_ds[parent_x] < -_ds[parent_y]) {
+                    swap(parent_x, parent_y);
+                }
+                _ds[parent_x] += _ds[parent_y];
+                _ds[parent_y] = parent_x;
+            }
+        }
+    };
+
+ public:
+    // clang-format off
+    /**
+     * FUNCTION DESCRIPTION
+     *
+     * Time complexity:
+     * Space complexity:
+     * Additional notes
+     *  -
+     */
+    // clang-format on
+    bool isBipartite(vector<vector<int>> &graph) {
+        int number_of_nodes        = static_cast<int>(graph.size());
+        unique_ptr<DisjointSet> ds = make_unique<DisjointSet>(number_of_nodes);
+        for (int idx = 0; idx < number_of_nodes; ++idx) {
+            int source_parent = ds->find(idx), node_parent = -1;
+            if (!graph[idx].empty()) {
+                node_parent = ds->find(graph[idx][0]);
+            }
+            for (int &neighbour : graph[idx]) {
+                int neighbour_parent = ds->find(neighbour);
+                node_parent          = ds->find(graph[idx][0]);
+                if (source_parent == neighbour_parent && source_parent != -1) {
+                    return false;
+                }
+                if (neighbour_parent >= 0 || node_parent >= 0) {
+                    ds->union_parents(neighbour_parent, node_parent);
+                }
+            }
+        }
+        return true;
+    }
+};
+}  // namespace solution_04
