c5a4c9fd55a1db09d8c3b4ac4265c640e4a14db8
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2021-09-19 12:10:11 +0530

    feat: added empty setup for bi-weekly contest 61 and weekly contest 259
    Signed-off-by: Sona Praneeth Akula <sonapraneeth.akula@gmail.com>

---
+++ platforms/leetcode/practice/non-premium/medium/2013-detect-squares/solutions.cpp
@@ -0,0 +1,80 @@
+// clang-format off
+/**
+ * COPYRIGHT NOTICE
+ *
+ * Copyright (c) 2021. Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
+ *
+ * This file is part of leetcode practice(submit) in competitive programming.
+ *
+ * This file cannot be copied and/or distributed without the express
+ * permission of the copyright owner.
+ *
+ */
+
+/**
+ * FILE DESCRIPTION
+ *
+ *        Filename: 2013__detect-squares/solutions.cpp
+ *      Created on: 19 September 2021
+ *   Last modified: 19 September 2021
+ *          Author: sonapraneeth_a
+ *     Description: LeetCode submission for 'Detect Squares' problem
+ */
+
+/**
+ * CHANGELOG
+ *
+ * Date (DD-MM-YYYY)            Author               Update
+ * 19-09-2021               sonapraneeth_a      - Creation of file
+ */
+// clang-format on
+
+namespace solution_01 {
+// clang-format off
+/**
+ * CLASS DESCRIPTION
+ *
+ * Submission link: https://leetcode.com/submissions/detail//
+ * Status: .  /  test cases passed.
+ * Runtime:  ms, faster than % of C++ online submissions
+ * Memory usage:  MB, less than % of C++ online submissions
+ * Time complexity:
+ * Space complexity:
+ * Tags:
+ * Categories:
+ * Additional notes
+ *  -
+ */
+// clang-format on
+// clang-format off
+/**
+ * FUNCTION DESCRIPTION
+ *
+ * Time complexity:
+ * Space complexity:
+ * Additional notes
+ *  -
+ */
+// clang-format on
+class DetectSquares {
+public:
+    DetectSquares() {
+        
+    }
+    
+    void add(vector<int> point) {
+        
+    }
+    
+    int count(vector<int> point) {
+        
+    }
+};
+
+/**
+ * Your DetectSquares object will be instantiated and called as such:
+ * DetectSquares* obj = new DetectSquares();
+ * obj->add(point);
+ * int param_2 = obj->count(point);
+ */
+}  // namespace solution_01
